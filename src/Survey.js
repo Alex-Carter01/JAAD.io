0e1a4ae56d8c99657025bd2bb8368f8632150b1c
Darius Barbano <darius.barbano@gmail.com>
2021-04-06 17:44:05 -0400

    new survey structure

--- src/Survey.js
+++ src/Survey.js
@@ -1,32 +1,94 @@
 import "./styles/Pages.css";
-import * as React from 'react';
-import {Button} from 'baseui/button';
+import * as React from "react";
+import { useState } from "react";
+import { Button } from "baseui/button";
+import { Radio, RadioGroup } from "baseui/radio";
+import { ArrowLeft, ArrowRight } from "baseui/icon";
 
-const SurveyStyle ={
+import { survey_data } from "./data/survey_data";
+
+const SurveyStyle = {
   textAlign: "left",
-  marginLeft:"40%",
-  color:"black",
-  fontSize:"120%"
-}
+  marginLeft: "40%",
+  color: "black",
+  fontSize: "120%",
+};
+
+const SurveyQuestion = (props) => {
+  return (
+    <div className="SurveyQuestion">
+      <p className="SurveyQuestionHeader">{props.question.question}</p>
+      <RadioGroup onChange={props.onChange} value={props.value}>
+        {props.question.answers.map((answer, index) => {
+          return (
+            <Radio type="radio" id="" value={index.toString()}>
+              {answer}
+            </Radio>
+          );
+        })}
+      </RadioGroup>
+    </div>
+  );
+};
+
+const createResponseArray = () => {
+  const arr = Array(survey_data.length);
+  arr.fill("0");
+  return arr;
+};
+
 const Survey = () => {
+  const [questionNumber, setQuestionNumber] = useState();
+  const [responses, setResponses] = useState(createResponseArray());
+
+  console.log(responses);
+
+  const changeQuestion = (direction) => {
+    if (direction == "left") {
+      setQuestionNumber(
+        questionNumber > 0 ? questionNumber - 1 : questionNumber
+      );
+    } else if (direction == "right") {
+      setQuestionNumber(
+        questionNumber < survey_data.length - 1
+          ? questionNumber + 1
+          : questionNumber
+      );
+    } else if (direction == "submit") {
+    }
+  };
+
+  if (!survey_data[questionNumber]) {
+    setQuestionNumber(0);
+  }
+
   return (
     <div className="Survey" style={SurveyStyle}>
-        <p>Are you able to perform physical activity?</p>
-        <input type="radio" id="yes" name="q1" value="yes"/>Yes
-        <br/>
-        <input type="radio" id="no" name="q1" value="no"/>No
-        <br/><br/>
-        <p>Do you enjoy working with animals?</p>
-        <input type="radio" id="yes" name="q2" value="yes"/>Yes
-        <br/>
-        <input type="radio" id="no" name="q2" value="no"/>No
-        <br/><br/>
-        <p>Do you enjoy a structured workplace?</p>
-        <input type="radio" id="yes" name="q3" value="yes"/>Yes
-        <br/>
-        <input type="radio" id="no" name="q3" value="no"/>No
-        <br/><br/>
-        <Button onClick={() => alert('check back later 🙂')}>Show My Results</Button>
+      {survey_data[questionNumber] && (
+        <SurveyQuestion
+          question={survey_data[questionNumber]}
+          value={responses[questionNumber]}
+          onChange={(e) => {
+            const new_responses = responses;
+            new_responses[questionNumber] = e.target.value;
+            setResponses([...new_responses]);
+          }}
+        />
+      )}
+      <Button onClick={() => changeQuestion("left")}>
+        <ArrowLeft size={64} />
+        text
+      </Button>
+      <Button
+        onClick={() => changeQuestion("submit")}
+        disabled={questionNumber != survey_data.length - 1}
+      >
+        Submit
+      </Button>
+      <Button onClick={() => changeQuestion("right")}>
+        <ArrowRight size={64} />
+        text
+      </Button>
     </div>
   );
 };
---
+++ src/data/survey_data.js
@@ -0,0 +1,14 @@
+export const survey_data = [
+  {
+    question: "Are you able to perform intense physical activity?",
+    answers: ["yes", "no"],
+  },
+  {
+    question: "Do you enjoy working with animals?",
+    answers: ["yes", "no"],
+  },
+  {
+    question: "Do you enjoy a structured workplace?",
+    answers: ["yes", "no"],
+  },
+];
